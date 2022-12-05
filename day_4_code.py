a75c74007833dc32be959bf7eec3c988293e104a
Zach Lefler <zachary.lefler@gmail.com>
2022-12-05 13:41:16 -0800

    finish day 4

--- day_4_code.py
+++ day_4_code.py
@@ -10,3 +10,13 @@ with open('day_4_input.txt') as f:
             overlaps += 1
             print(assignments)
 print(f'answer to Part One: {overlaps}')
+
+with open('day_4_input.txt') as f:
+    overlaps = 0
+    for line in f.read().splitlines():
+        assignments = re.split('-|,', line)
+        if int(assignments[0]) <= int(assignments[3]) and int(assignments[0]) >= int(assignments[2]):
+            overlaps += 1
+        elif int(assignments[2]) <= int(assignments[1]) and int(assignments[2]) >= int(assignments[0]):
+            overlaps += 1
+print(f'answer to Part Two: {overlaps}')

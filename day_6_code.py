45c1611c148afdea24d18a6470066e489da2d39e
Zach Lefler <zachary.lefler@gmail.com>
2022-12-06 12:22:18 -0800

    finish day 6

--- day_6_code.py
+++ day_6_code.py
@@ -1,20 +1,23 @@
-def marker_start() -> int:  # Function for Part One
+def marker_start(k, sample=None) -> int:
     '''...It needs to report the number of characters from the beginning of the buffer to the end of the first such four-character marker.'''
     frequency_map = {}
     left = 0
     with open('day_6_input.txt') as f:
-        text = f.read()
+        if sample is None:
+            text = f.read()
+        else:
+            text = sample
         for right in range(len(text)):
             frequency_map[text[right]] = frequency_map.get(text[right], 0) + 1
-            if right == 3 and len(frequency_map) == 4:
+            if right == k-1 and len(frequency_map) == k:
                 return left
-            elif right > 3:
+            elif right >= k:
                 frequency_map[text[left]] -= 1
                 if frequency_map[text[left]] == 0:
                     del frequency_map[text[left]]
                 left += 1
-                if len(frequency_map) == 4:
-                    return left + 4
+                if len(frequency_map) == k:
+                    return left + k
 
     # set left and right sides of window
     # move right side k times, add letters to map
@@ -23,4 +26,5 @@ def marker_start() -> int:  # Function for Part One
     # check again, when all discrete, return left
 
 
-print(f'answer to Part One: {marker_start()}')
+print(f'answer to Part One: {marker_start(4)}')
+print(f'answer to Part Two: {marker_start(14)}')
